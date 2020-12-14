61d320d96878b31fb1afff22fc540974765251c7
Sean Morgan <insideseanshead@gmail.com>
2020-12-13 18:06:12 -0800

    Set up initial html and js, psudo coded out js and npm init and install express

---
+++ server.js
@@ -0,0 +1,19 @@
+//Dependencies
+
+//Express Setup
+
+//Express data parsing
+
+//gaslands cars(data)
+
+//routes
+
+//first ajax page "/"
+
+//display all cars
+
+//display single car or return false
+
+//create new car 
+
+//start server 
